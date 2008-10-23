56840aa929d2e2e1fb2bdd75735a745d3b31a7ab
Robbie <kitserver@robv.de>
2008-10-23 23:39:51 +0000

    very small proof-of-concept for kserv

--- src/kserv.h
+++ src/kserv.h
@@ -1,8 +1,5 @@
 // kserv.h
 
-#include "afsreader.h"
-#include "teaminfo.h"
-
 #define MODID 100
 #define NAMELONG L"Kitserver Module 7.3.1.5"
 #define NAMESHORT L"KSERV"
@@ -10,38 +7,41 @@
 
 #pragma pack(4)
 
+typedef DWORD (*INIT_NEW_KIT)(DWORD);
+
+KEXPORT DWORD hookVtableFunction(DWORD vtableAddr, DWORD offset, void* func);
+
+
 typedef struct _PNG_CHUNK_HEADER {
     DWORD dwSize;
     DWORD dwName;
 } PNG_CHUNK_HEADER;
 
-typedef struct _TEXTURE_ENTRY_HEADER
-{
-    BYTE sig[4]; // "WE00"
-    DWORD unknown1;
-    WORD width;
-    WORD height;
-    DWORD unknown2;
-} TEXTURE_ENTRY_HEADER;
+typedef struct _PES_TEXTURE {
+    DWORD* vtable;	// == 0xffab80 for PES2009
+	BYTE unknown1[8];
+	DWORD width;
+	DWORD height;
+	BYTE unknown2[0xc];
+	IDirect3DTexture9* tex;
+} PES_TEXTURE;
 
-typedef struct _PALETTE_ENTRY
-{
-    BYTE b;
-    BYTE g;
-    BYTE r;
-    BYTE a;
-} PALETTE_ENTRY;
+typedef struct _PES_TEXTURE_PACKAGE {
+    DWORD* vtable;	// == 0x1009df8 for PES2009
+	BYTE unknown1[8];
+	PES_TEXTURE* tex;
+} PES_TEXTURE_PACKAGE;
 
-typedef struct _TEXTURE_ENTRY
-{
-    TEXTURE_ENTRY_HEADER header;
-    PALETTE_ENTRY palette[256];
-    BYTE data[1];
-} TEXTURE_ENTRY;
-
-typedef struct _BIN_INFO
-{
-    DWORD id;
-    AFSITEMINFO itemInfo;
-} BIN_INFO;
+typedef struct _KIT_OBJECT {
+    DWORD* vtable;	// == code[C_KIT_VTABLE]
+	BYTE unknown1[0x40];
+	PES_TEXTURE_PACKAGE* kitTex; 
+	PES_TEXTURE_PACKAGE* flagTex; 
+	BYTE unknown2[0x30];
+	WORD teamId;
+	BYTE unknown3[6];
+	BYTE isAfsKit;
+    BYTE unknown4[3];
+	DWORD kitIdx;
+} KIT_OBJECT;
 
